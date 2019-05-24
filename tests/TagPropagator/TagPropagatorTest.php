19ec4af742233a65d197cf2e02e44245201317d8
Sven Speckmaier <sps@ipunkt.biz>
2019-05-24 22:59:50 +0200

    Test: TagPropagator

---
+++ tests/TagPropagator/TagPropagatorTest.php
@@ -0,0 +1,57 @@
+<?php namespace Ipunkt\LaravelJaegerTests\TagPropagator;
+
+use Ipunkt\LaravelJaeger\TagPropagator\TagPropagator;
+use Ipunkt\LaravelJaegerTests\TestCase;
+
+/**
+ * Class TagPropagatorTest
+ * @package Ipunkt\LaravelJaegerTests\TagPropagator
+ */
+class TagPropagatorTest extends TestCase {
+
+	/**
+	 * @var TagPropagator
+	 */
+	protected $tagPropagator;
+
+	public function setUp(): void {
+		parent::setUp();
+		$this->tagPropagator = new TagPropagator();
+	}
+
+	/**
+	 * @test
+	 */
+	public function injectsTags() {
+		$this->tagPropagator->addTags([
+			'tag1' => 'value1'
+		]);
+
+		$data = [];
+		$this->tagPropagator->inject($data);
+
+		$this->assertArrayHasKey('propagated-tags', $data);
+		$this->assertArrayHasKey('tag1', $data['propagated-tags']);
+		$this->assertEquals('value1', $data['propagated-tags']['tag1']);
+	}
+
+	/**
+	 * @test
+	 */
+	public function extractsTags() {
+
+		$data = [
+			'propagated-tags' => [
+				'tag1' => 'value1'
+			]
+		];
+
+		$this->tagPropagator->extract($data);
+
+		$resultData = [];
+		$this->tagPropagator->inject($resultData);
+		$this->assertArrayHasKey('propagated-tags', $resultData);
+		$this->assertArrayHasKey('tag1', $resultData['propagated-tags']);
+		$this->assertEquals('value1', $resultData['propagated-tags']['tag1']);
+	}
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,9 @@
+<?php namespace Ipunkt\LaravelJaegerTests;
+
+/**
+ * Class TestCase
+ * @package Ipunkt\LaravelJaegerTests
+ */
+class TestCase extends \PHPUnit\Framework\TestCase {
+
+}
