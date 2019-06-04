272af5e7d47d11fef3677085f30ed0f3aafe3243
Robert Kummer <github@mail.robert-kummer.de>
2019-06-04 16:12:02 +0200

    Added mergeFromConfig to prevent null assignment

--- src/Provider.php
+++ src/Provider.php
@@ -1,14 +1,13 @@
 <?php namespace Ipunkt\LaravelJaeger;
 
-use Artisan;
+use DB;
+use Event;
 use Illuminate\Foundation\Http\Events\RequestHandled;
 use Illuminate\Log\Events\MessageLogged;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Ipunkt\LaravelJaeger\Context\EmptyContext;
 use Ipunkt\LaravelJaeger\Context\SpanContext;
-use Event;
-use DB;
 use Ipunkt\LaravelJaeger\Context\TracerBuilder\TracerBuilder;
 use Jaeger\Config;
 use Log;
@@ -19,46 +18,50 @@ use Log;
  */
 class Provider extends ServiceProvider
 {
-
     public function register()
     {
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/jaeger.php', 'jaeger'
+        );
+
         $this->publishes([
-            __DIR__.'/config/jaeger.php' => config_path('jaeger.php'),
+            __DIR__ . '/config/jaeger.php' => config_path('jaeger.php'),
         ]);
 
-        $this->app->singleton(Config::class, function() {
-        	$config = Config::getInstance();
-        	$config->gen128bit();
-	        return $config;
+        $this->app->singleton(Config::class, function () {
+            $config = Config::getInstance();
+            $config->gen128bit();
+            return $config;
         });
 
-        $this->app->resolving(TracerBuilder::class, function(TracerBuilder $tracerBuilder) {
-        	$tracerBuilder
-		        ->setName(config('app.name'))
-		        ->setJaegerHost(config('jaeger.host'));
-        	return $tracerBuilder;
+        $this->app->resolving(TracerBuilder::class, function (TracerBuilder $tracerBuilder) {
+            $tracerBuilder
+                ->setName(config('app.name'))
+                ->setJaegerHost(config('jaeger.host'));
+            return $tracerBuilder;
         });
 
         // Setup a unique ID for each request. This will allow us to find
         // the request trace in the jaeger ui
-        $this->app->instance('context', app(EmptyContext::class) );
+        $this->app->instance('context', app(EmptyContext::class));
     }
 
     public function boot()
     {
         $this->setupQueryLogging();
 
-        if( app()->runningInConsole() && $this->disabledInConsole() )
+        if (app()->runningInConsole() && $this->disabledInConsole()) {
             return;
+        }
 
-        $this->app->instance('context', app(SpanContext::class) );
-    	app('context')->start();
+        $this->app->instance('context', app(SpanContext::class));
+        app('context')->start();
 
 
-	    $this->registerEvents();
+        $this->registerEvents();
 
-	    $this->parseRequest();
-	    $this->parseCommand();
+        $this->parseRequest();
+        $this->parseCommand();
     }
 
     protected function registerEvents(): void
@@ -66,17 +69,17 @@ class Provider extends ServiceProvider
         // When the app terminates we must finish the global span
         // and send the trace to the jaeger agent.
         app()->terminating(function () {
-	        app('context')->finish();
+            app('context')->finish();
         });
 
         // Listen for each logged message and attach it to the global span
         Event::listen(MessageLogged::class, function (MessageLogged $e) {
-	        app('context')->log((array)$e);
+            app('context')->log((array)$e);
         });
 
         // Listen for the request handled event and set more tags for the trace
         Event::listen(RequestHandled::class, function (RequestHandled $e) {
-	        app('context')->setPrivateTags([
+            app('context')->setPrivateTags([
                 'user_id' => optional(auth()->user())->id ?? "-",
                 'company_id' => optional(auth()->user())->company_id ?? "-",
 
@@ -91,41 +94,43 @@ class Provider extends ServiceProvider
         });
     }
 
-	private function setupQueryLogging() {
+    private function setupQueryLogging()
+    {
 
-		// Also listen for queries and log then,
-		// it also receives the log in the MessageLogged event above
-		DB::listen(function ($query) {
-			Log::debug("[DB Query] {$query->connection->getName()}", [
-				'query' => str_replace('"', "'", $query->sql),
+        // Also listen for queries and log then,
+        // it also receives the log in the MessageLogged event above
+        DB::listen(function ($query) {
+            Log::debug("[DB Query] {$query->connection->getName()}", [
+                'query' => str_replace('"', "'", $query->sql),
                 'bindings' => $query->bindings,
-				'time' => $query->time . 'ms',
-			]);
-		});
-	}
+                'time' => $query->time . 'ms',
+            ]);
+        });
+    }
 
-	private function parseRequest() {
+    private function parseRequest()
+    {
 
-		if ( app()->runningInConsole() )
-		    return;
+        if (app()->runningInConsole()) {
+            return;
+        }
 
-		app('context')->parse( request()->url(), request()->input() );
-	}
+        app('context')->parse(request()->url(), request()->input());
+    }
 
     private function parseCommand()
     {
-        if ( !app()->runningInConsole() ) {
+        if (!app()->runningInConsole()) {
             return;
         }
 
         $currentArgs = request()->server('argv');
         $commandLine = implode(' ', $currentArgs);
-        app('context')->parse( $commandLine, [] );
+        app('context')->parse($commandLine, []);
     }
 
     private function disabledInConsole()
     {
         return !config('jaeger.enable-for-console');
     }
-
 }
