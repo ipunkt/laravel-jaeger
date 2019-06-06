0dee49f265bbb54a5f8fd2f2165e216f294a7991
svensp <sps@ipunkt.biz>
2019-06-06 13:35:06 +0200

    changed default host to `localhost`

--- src/config/jaeger.php
+++ src/config/jaeger.php
@@ -1,7 +1,7 @@
 <?php
 return [
 
-    'host' => env('JAEGER_AGENT_HOST', 'jaeger').':'.env('JAEGER_AGENT_PORT', 6831),
+    'host' => env('JAEGER_AGENT_HOST', 'localhost').':'.env('JAEGER_AGENT_PORT', 6831),
 
     'enable-for-console' => env('JAEGER_ENABLE_FOR_CONSOLE', true),
 ];
