18910950ca6178415348294788f7f0dbc014504b
svensp <sps@ipunkt.biz>
2019-06-19 14:48:02 +0200

    #1 default value for `JAEGER_ENABLE_FOR_CONSOLE` is now `false`

--- src/config/jaeger.php
+++ src/config/jaeger.php
@@ -3,5 +3,5 @@ return [
 
     'host' => env('JAEGER_AGENT_HOST', 'localhost').':'.env('JAEGER_AGENT_PORT', 6831),
 
-    'enable-for-console' => env('JAEGER_ENABLE_FOR_CONSOLE', true),
+    'enable-for-console' => env('JAEGER_ENABLE_FOR_CONSOLE', false),
 ];
